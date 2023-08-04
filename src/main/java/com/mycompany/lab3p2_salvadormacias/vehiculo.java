b8ac37a07017fac582bfdc5e1450c93db5ae6a65
papilon10 <140213599+papilon10@users.noreply.github.com>
2023-08-04 14:34:55 -0600

    Add files via upload
    commit inicial

--- src/main/java/com/mycompany/lab3p2_salvadormacias/vehiculo.java
+++ src/main/java/com/mycompany/lab3p2_salvadormacias/vehiculo.java
@@ -4,10 +4,19 @@
  */
 package com.mycompany.lab3p2_salvadormacias;
 
+import java.awt.Color;
+import java.util.Date;
+
 /**
  *
  * @author papilon10
  */
 public class vehiculo {
+    int numero_placa;
+    String marca;
+    String modelo;
+    String tipo;
+    Color color;
+    Date yaer;
     
 }
