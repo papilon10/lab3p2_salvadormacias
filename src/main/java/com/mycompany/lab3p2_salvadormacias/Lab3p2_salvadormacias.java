fea47455cf76fb09fb9f14b9a87e50098b292421
papilon10 <140213599+papilon10@users.noreply.github.com>
2023-08-04 14:30:34 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/lab3p2_salvadormacias/Lab3p2_salvadormacias.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab3p2_salvadormacias;
+
+/**
+ *
+ * @author papilon10
+ */
+public class Lab3p2_salvadormacias {
+
+    public static void main(String[] args) {
+        int opcion;
+
+    }
+}
