7077ab7378e04eda13584d15dd7fe36c6ac57cd6
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-02-02 16:09:05 -0600

    inicio

---
+++ src/main/java/www/lab3p2_salvadormacias/Lab3p2_salvadormacias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package www.lab3p2_salvadormacias;
+
+/**
+ *
+ * @author Apple
+ */
+public class Lab3p2_salvadormacias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
